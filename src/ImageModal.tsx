4c5d314e0acd87081d9654e37172c2cb6985d98f
Amal221200 <amalmurikkoli2000@gmail.com>
2024-07-18 15:56:45 +0530

    responsive

--- src/ImageModal.tsx
+++ src/ImageModal.tsx
@@ -39,15 +39,15 @@ const ImageModal = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [openedImage])
     return (
-        <div id="image-modal" className={`absolute inset-0 w-full h-full bg-black/70 justify-center items-center ${open ? 'flex' : 'hidden'}`}>
+        <div id="image-modal" className={`absolute overflow-hidden inset-0 w-full h-full bg-black/70 justify-center items-center ${open ? 'flex' : 'hidden'}`}>
             <button type="button" className="absolute right-8 top-8 bg-transparent" onClick={() => onClose()}>
                 <XIcon className="scale-150 text-white/60 transition-colors hover:text-white" />
             </button>
 
-            <div className="flex h-full flex-col justify-between space-y-2 px-8 pb-3 sm:px-3">
+            <div className="flex h-full flex-col justify-between space-y-2 px-3 pb-3">
                 <div />
-                <div className="mx-auto max-w-sm lg:max-w-3xl" key={openedImage}>
-                    <img src={images[openedImage!]} className="w-full rounded" />
+                <div className="mx-auto max-w-xs px-8 sm:max-w-md md:max-w-xl lg:max-w-3xl" key={openedImage}>
+                    <img src={images[openedImage!]} draggable={false} className="w-full rounded" />
                 </div>
 
                 <button disabled={openedImage === 0} onClick={() => onOpen(openedImage! - 1)} type="button" className="absolute left-5 top-[50%] translate-y-[-50%] text-white/50 transition-colors hover:text-white disabled:text-transparent">
@@ -58,7 +58,7 @@ const ImageModal = () => {
                     <ChevronRight className="scale-150" />
                 </button>
 
-                <div className="scrollbar mx-auto flex max-w-[10rem] flex-row gap-2 overflow-x-auto sm:max-w-xl">
+                <div className="scrollbar mx-auto flex w-full flex-row gap-2 overflow-x-auto px-4 sm:max-w-xl">
                     {
                         images.map((image, idx) => (
                             <img src={image} key={idx} onClick={() => onOpen(idx)} className={`object-cover cursor-pointer object-center w-20 sm:w-28 rounded-md ${idx === openedImage && 'border-2 opacity-50 '} `} draggable={false} />
