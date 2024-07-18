a0a8de8d283db0d63aa11b48974a567e4cded07f
Amal221200 <amalmurikkoli2000@gmail.com>
2024-07-18 16:37:42 +0530

    touch gesture

--- src/ImageModal.tsx
+++ src/ImageModal.tsx
@@ -1,7 +1,7 @@
 import { ChevronLeft, ChevronRight, XIcon } from "lucide-react"
 import { images } from "./data"
 import useImageModal from "./useImageModal"
-import { useCallback, useEffect } from "react"
+import { useCallback, useEffect, useRef } from "react"
 
 const ImageModal = () => {
     const { open, imageIndex: openedImage, onOpen, onClose } = useImageModal()
@@ -20,6 +20,7 @@ const ImageModal = () => {
         onOpen(openedImage! - 1)
     }, [onOpen, openedImage])
 
+    // Desktop Events
     useEffect(() => {
         const handleKeyNavigation = (e: KeyboardEvent) => {
             if (e.key === 'ArrowLeft') {
@@ -38,27 +39,63 @@ const ImageModal = () => {
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [openedImage])
+
+    const touchPosition = useRef(0)
+    const isPrev = useRef(false)
+    const isNext = useRef(false)
+// Mobile Events
+    useEffect(() => {
+
+        const handleTouchStart = (e: TouchEvent) => {
+            // console.log(e.touches[0]);
+            touchPosition.current = e.touches[0].clientX
+            return [isNext.current, isPrev.current] = [false, false]
+        }
+
+        const handleTouchMove = (e: TouchEvent) => {
+            isPrev.current = (touchPosition.current < e.touches[0].clientX)
+            isNext.current = (touchPosition.current > e.touches[0].clientX)
+        }
+
+        const handleTouchEnd = () => {
+            if (isPrev.current) {
+                handlePrev()
+            } else if (isNext.current) {
+                handleNext()
+            }
+        }
+
+        document.addEventListener('touchstart', handleTouchStart)
+        document.addEventListener('touchmove', handleTouchMove)
+        document.addEventListener('touchend', handleTouchEnd)
+        return () => {
+            document.removeEventListener('touchstart', handleTouchStart)
+            document.removeEventListener('touchmove', handleTouchMove)
+            document.removeEventListener('touchend', handleTouchEnd)
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [openedImage])
     return (
         <div id="image-modal" className={`absolute overflow-hidden inset-0 w-full h-full bg-black/70 justify-center items-center ${open ? 'flex' : 'hidden'}`}>
             <button type="button" className="absolute right-8 top-8 bg-transparent" onClick={() => onClose()}>
                 <XIcon className="scale-150 text-white/60 transition-colors hover:text-white" />
             </button>
 
-            <div className="flex h-full flex-col justify-between space-y-2 px-3 pb-3">
+            <div className="flex h-full flex-col justify-between space-y-2 px-5 pb-3">
                 <div />
-                <div className="mx-auto max-w-xs px-8 sm:max-w-md md:max-w-xl lg:max-w-3xl" key={openedImage}>
+                <div className="mx-auto max-w-sm sm:max-w-md md:max-w-xl lg:max-w-3xl" key={openedImage}>
                     <img src={images[openedImage!]} draggable={false} className="w-full rounded" />
                 </div>
 
-                <button disabled={openedImage === 0} onClick={() => onOpen(openedImage! - 1)} type="button" className="absolute left-5 top-[50%] translate-y-[-50%] text-white/50 transition-colors hover:text-white disabled:text-transparent">
+                <button disabled={openedImage === 0} onClick={() => onOpen(openedImage! - 1)} type="button" className="absolute left-5 top-[50%] hidden translate-y-[-50%] text-white/50 transition-colors hover:text-white disabled:text-transparent sm:block">
                     <ChevronLeft className="scale-150" />
                 </button>
 
-                <button disabled={openedImage === (images.length - 1)} onClick={() => onOpen(openedImage! + 1)} type="button" className="absolute right-5 top-[50%] translate-y-[-50%] text-white/50 transition-colors hover:text-white disabled:text-transparent">
+                <button disabled={openedImage === (images.length - 1)} onClick={() => onOpen(openedImage! + 1)} type="button" className="absolute right-5 top-[50%] hidden translate-y-[-50%] text-white/50 transition-colors hover:text-white disabled:text-transparent sm:block">
                     <ChevronRight className="scale-150" />
                 </button>
 
-                <div className="scrollbar mx-auto flex w-full flex-row gap-2 overflow-x-auto px-4 sm:max-w-xl">
+                <div className="scrollbar mx-auto flex w-full flex-row gap-2 overflow-x-auto sm:max-w-xl">
                     {
                         images.map((image, idx) => (
                             <img src={image} key={idx} onClick={() => onOpen(idx)} className={`object-cover cursor-pointer object-center w-20 sm:w-28 rounded-md ${idx === openedImage && 'border-2 opacity-50 '} `} draggable={false} />
