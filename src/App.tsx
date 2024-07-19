428aaab1b4aa308b40a0e7203c3c720b12b83cea
Amal221200 <amalmurikkoli2000@gmail.com>
2024-07-19 12:39:24 +0530

    small optimiztions on App component keyboard navigation

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,14 @@ import { useEffect, useRef } from "react"
 
 function App() {
   const { onOpen, open, imageIndex } = useImageModal()
-  const id = useRef(-1)
+  const selectedId = useRef(-1)
 
   useEffect(() => {
     if (open) {
       // To remember the selected image before closing the slider
-      const newIndex = imageIndex ?? id.current
+      const newIndex = imageIndex ?? selectedId.current
       document.querySelector<HTMLButtonElement>(`#image-${newIndex}`)?.focus()
-      id.current = newIndex
+      selectedId.current = newIndex
       return
     }
 
@@ -33,28 +33,28 @@ function App() {
       }
 
       if (e.key === 'ArrowLeft') {
-        const newId = id.current > 0 ? id.current - 1 : 0
+        const newId = selectedId.current === 0 ? images.length - 1 : selectedId.current - 1
         document.querySelector<HTMLButtonElement>(`#image-${newId}`)?.focus()
-        id.current = newId
+        selectedId.current = newId
       } else if (e.key === 'ArrowRight') {
-        const newId = id.current < images.length - 1 ? id.current + 1 : images.length - 1
+        const newId = selectedId.current === (images.length -1) ? 0 : selectedId.current + 1
         document.querySelector<HTMLButtonElement>(`#image-${newId}`)?.focus()
-        id.current = newId
+        selectedId.current = newId
       } else if (e.key === 'ArrowUp') {
-        const potentialId = id.current === -1 ? images.length - 1 : id.current - cols
+        const potentialId = selectedId.current === -1 ? images.length - 1 : selectedId.current - cols
         const newId = potentialId > 0 ? potentialId : 0
         document.querySelector<HTMLButtonElement>(`#image-${newId}`)?.focus()
-        id.current = newId
+        selectedId.current = newId
       }
       else if (e.key === 'ArrowDown') {
-        const potentialId = id.current === -1 ? 0 : id.current + cols
+        const potentialId = selectedId.current === -1 ? 0 : selectedId.current + cols
         const newId = potentialId < images.length - 1 ? potentialId : images.length - 1
         document.querySelector<HTMLButtonElement>(`#image-${newId}`)?.focus()
-        id.current = newId
+        selectedId.current = newId
       }
       else if (e.key === 'Escape') {
-        document.querySelector<HTMLButtonElement>(`#image-${id.current}`)?.blur()
-        id.current = -1
+        document.querySelector<HTMLButtonElement>(`#image-${selectedId.current}`)?.blur()
+        selectedId.current = -1
       }
     }
 
@@ -69,7 +69,7 @@ function App() {
       {
         images.map((image, idx) => (
           <article key={image} className="group/container relative cursor-pointer overflow-hidden">
-            <button id={`image-${idx}`} onBlur={() => id.current = -1} onClick={() => onOpen(idx)} className="group/button h-full w-full focus:ring-white focus:ring-offset-2 focus-visible:ring-white focus-visible:ring-offset-2">
+            <button id={`image-${idx}`} onBlur={() => selectedId.current = -1} onClick={() => onOpen(idx)} className="group/button h-full w-full focus:ring-white focus:ring-offset-2 focus-visible:ring-white focus-visible:ring-offset-2">
               <img src={image} className="h-full w-full object-cover object-center transition-transform ease-linear group-hover/container:scale-150 group-focus/button:scale-150" />
               <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center bg-transparent transition-colors group-hover/container:bg-black/40 group-focus/button:bg-black/40">
                 <MoveDiagonalIcon
